b1c0e79a1de54d6b0e3fd5b5be8dd0449794bc09
Aaron <junewhj@qq.com>
2025-08-02 03:26:10 +0800

    v3.3.2

--- mkdocs_document_dates/static/core/timeago-load.js
+++
@@ -1,5 +0,0 @@
-if (typeof window.document$ !== 'undefined' && !window.document$.isStopped) {
-    window.document$.subscribe(window.renderDocumentDates);
-} else {
-    window.renderDocumentDates();
-}
